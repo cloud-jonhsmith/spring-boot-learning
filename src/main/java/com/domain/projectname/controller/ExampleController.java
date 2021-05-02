dd2e10dc27b693a4d9dc10470133845826df88c9
Jonhscz <jonhsmith.cordova@gmail.com>
2021-05-01 22:15:48 -0500

    Cambiamos el @RequestMapping por @GetMapping que es una anotación hijo de @RequestMapping, el cuál ya lleva asociada el method=RequesMethod.GET

--- src/main/java/com/domain/projectname/controller/ExampleController.java
+++ src/main/java/com/domain/projectname/controller/ExampleController.java
@@ -1,23 +1,24 @@
 package com.domain.projectname.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/example") //localhost:8080/example
+@RequestMapping("example") //localhost:8080/example
 public class ExampleController {
 	
 	//Primera forma
-	@RequestMapping(value="/exampleString", method = RequestMethod.GET)
+	@GetMapping("exampleString")
 	//localhost:8080/example/exampleString
 	public String exampleString() {
 		return "example"; // retorna el nombre de la vista ubicada en templates
 	}
 	
 	//Segunda forma
-	@RequestMapping(value="/exampleMAV", method = RequestMethod.GET)
+	@GetMapping("exampleMAV")
 	//localhost:8080/example/exampleMAV
 	public ModelAndView exampleMAV() {
 		return new ModelAndView("example");
