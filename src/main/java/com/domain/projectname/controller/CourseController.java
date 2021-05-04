a2f77523b77e757889fc61e2f6d9822c969b0ad4
Jonhscz <jonhsmith.cordova@gmail.com>
2021-05-04 17:39:44 -0500

    Creando e integrando todas las capas - La vista

--- src/main/java/com/domain/projectname/controller/CourseController.java
+++ src/main/java/com/domain/projectname/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.domain.projectname.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -18,19 +20,24 @@ public class CourseController {
 	
 	private static final String COURSES_VIEW = "courses";
 	
+	private static final Log LOG = LogFactory.getLog(CourseController.class);
+	
 	@Autowired
 	@Qualifier("courseServiceImpl")
 	private CourseService courseService;
 	
 	@GetMapping("/listcourses")
 	public ModelAndView listAllCourses() {
+		LOG.info("Call: " + "listAllCourses()");
 		ModelAndView mav = new ModelAndView(COURSES_VIEW);
+		mav.addObject("course", new Course());
 		mav.addObject("courses", courseService.listAllCourses());
 		return mav;
 	}
 	
 	@PostMapping("/addcourse")
 	public String addCourse(@ModelAttribute("course") Course course) {
+		LOG.info("Call: " + "addCourse()" + " -- Param: " + course.toString());
 		courseService.addCourse(course);
 		return "redirect:/courses/listcourses";
 	}
