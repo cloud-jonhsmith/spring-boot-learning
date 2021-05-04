e19d5e33e8f06e41167959971d85edb9adaa99df
Jonhscz <jonhsmith.cordova@gmail.com>
2021-05-04 13:11:52 -0500

    Creando entidades Hibernate, creamos una clase Course que la anotaremos como @Entity, esta clase contendrá los campos de una tabla en la base de datos

---
+++ src/main/java/com/domain/projectname/entity/Course.java
@@ -0,0 +1,84 @@
+package com.domain.projectname.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+	@Id
+	@GeneratedValue
+	@Column(name="id")	// Si el name fuese "idCourse", el dialecto de hibernate lo
+						// cambiaría a "id_course" en la base de datos,
+						// por ello solo colocamos "id"
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="price")
+	private int price;
+	
+	@Column(name="hours")
+	private int hours;
+
+	public Course() {
+	}
+
+	public Course(int id, String name, String description, int price, int hours) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.hours = hours;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+}
